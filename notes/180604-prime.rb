6b3e1f23a1445c78a4b454b66e0e0ca16657bfcc
zldang <38782010+zldang@users.noreply.github.com>
2018-06-05 01:37:50 +0700

    optimize prime finder

--- notes/180604-prime.rb
+++ notes/180604-prime.rb
@@ -1,11 +1,21 @@
 a = 100
 b = (2...a)
+c = (3...a)
 b.each do |x|
   prime = x
-  b.each do |y|
-    if  y * y < x then
-      if x % y == 0
-        prime = nil
+  if x % 2 == 0 && x > 2 then 
+    prime = nil
+  end
+  if x == 2 then 
+    prime = x 
+  elsif x == 3 then 
+    prime = x 
+  else  
+    c.each do |y|
+      if  y * y < x then
+        if x % y == 0
+          prime = nil
+        end
       end
     end
   end
