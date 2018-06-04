dc4497d45ff42cebdd81be678b89584443d9d217
zldang <38782010+zldang@users.noreply.github.com>
2018-06-05 00:34:27 +0700

    rb formatting

--- notes/180604-fizzbuzz.rb
+++ notes/180604-fizzbuzz.rb
@@ -1,10 +1,10 @@
-a=[1,2,3,4,5,6]
+a = [1, 2, 3, 4, 5, 6]
 a.each do |x|
-  if x%2 == 0 && x%3 == 0
-    puts "fizz buzz"
-  elsif x%2 == 0
-    puts "buzz"
-  elsif x%3 == 0
-    puts "fizz"
+  if x.even? && x % 3 == 0
+    puts 'fizz buzz'
+  elsif x.even?
+    puts 'buzz'
+  elsif x % 3 == 0
+    puts 'fizz'
   end
 end
--- notes/180604-ptb2.rb
+++ notes/180604-ptb2.rb
@@ -1,28 +1,28 @@
-a=1
-b=0
-c=3
-if a==0 then
-  if b==0 then
-    if c==0 then
-      puts "Vo so nghiem"
+a = 1
+b = 0
+c = 3
+if a == 0
+  if b == 0
+    if c == 0
+      puts 'Vo so nghiem'
     else
-      puts "Vo nghiem"
+      puts 'Vo nghiem'
     end
   else
-    x = -c/b
-    puts "1 nghiem: "+x.to_s
+    x = -c / b
+    puts '1 nghiem: ' + x.to_s
   end
 else
-  d=b*b-4*a*c
-  if d<0
-    puts "Vo nghiem"
-  elsif d==0
-    x = (-b)/(2*a)
-    puts "1 nghiem: " + x.to_s
+  d = b * b - 4 * a * c
+  if d < 0
+    puts 'Vo nghiem'
+  elsif d == 0
+    x = (-b) / (2 * a)
+    puts '1 nghiem: ' + x.to_s
   else
-    x1 = (-b+Math.sqrt(d))/(2*a)
-    x2 = (-b-Math.sqrt(d))/(2*a)
-    puts "Nghiem 1: "+x1.to_s
-    puts "Nghiem 2: "+x2.to_s
+    x1 = (-b + Math.sqrt(d)) / (2 * a)
+    x2 = (-b - Math.sqrt(d)) / (2 * a)
+    puts 'Nghiem 1: ' + x1.to_s
+    puts 'Nghiem 2: ' + x2.to_s
   end
 end
