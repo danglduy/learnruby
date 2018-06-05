d3c3694f4a006bd423bb14dfefdc154cf86b700c
zldang <38782010+zldang@users.noreply.github.com>
2018-06-06 00:51:02 +0700

    reformat

--- notes/180604-R/180604-prime.rb
+++ notes/180604-R/180604-prime.rb
@@ -1,19 +1,19 @@
 a = 100
 b = (2...a)
-c = (3...Math.sqrt(a).floor).delete_if{|n| n % 2 == 0}
+c = (3...Math.sqrt(a).floor).delete_if { |n| n % 2 == 0 }
 b.each do |x|
   prime = x
-  if x % 2 == 0 && x > 2 then 
+  if x % 2 == 0 && x > 2
     prime = nil
   end
-  if x == 2 then 
-    prime = x 
-  elsif x == 3 then 
-    prime = x 
-  else  
+  if x == 2
+    prime = x
+  elsif x == 3
+    prime = x
+  else
     c.each do |y|
-      if prime != nil
-        if y < x then
+      if !prime.nil?
+        if y < x
           if x % y == 0
             prime = nil
           end
@@ -21,7 +21,7 @@ b.each do |x|
       end
     end
   end
-  if prime != nil then
+  if !prime.nil?
     puts prime.to_s
   end
 end
--- notes/180604-R/180604-ptb2.rb
+++ notes/180604-R/180604-ptb2.rb
@@ -4,25 +4,25 @@ c = 3
 if a == 0
   if b == 0
     if c == 0
-      puts 'Vo so nghiem'
+      puts "Vo so nghiem"
     else
-      puts 'Vo nghiem'
+      puts "Vo nghiem"
     end
   else
     x = -c / b
-    puts '1 nghiem: ' + x.to_s
+    puts "1 nghiem: " + x.to_s
   end
 else
   d = b * b - 4 * a * c
   if d < 0
-    puts 'Vo nghiem'
+    puts "Vo nghiem"
   elsif d == 0
     x = (-b) / (2 * a)
-    puts '1 nghiem: ' + x.to_s
+    puts "1 nghiem: " + x.to_s
   else
     x1 = (-b + Math.sqrt(d)) / (2 * a)
     x2 = (-b - Math.sqrt(d)) / (2 * a)
-    puts 'Nghiem 1: ' + x1.to_s
-    puts 'Nghiem 2: ' + x2.to_s
+    puts "Nghiem 1: " + x1.to_s
+    puts "Nghiem 2: " + x2.to_s
   end
 end
