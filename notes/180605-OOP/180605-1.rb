aa9f305c3455d1253a27a5f3944c9a51dffb02ca
zldang <38782010+zldang@users.noreply.github.com>
2018-06-05 20:16:43 +0700

    OOP

---
+++ notes/180605-OOP/180605-1.rb
@@ -0,0 +1,20 @@
+# Define class (object prototype)
+class vios20
+  def drive
+    
+
+  end
+end
+
+v1 = vios20.new # Create object
+v2 = vios20.new
+
+v1.counter = 5000 # Change property/attribute
+v2.counter = 50000
+
+# 1.Change property: Use behavior/method to change property
+# 2.OR: Change property directly
+
+# Two object same class: Classified by property/attribute
+# Two class: Classified by behavior/method
+
---
+++ notes/180605-OOP/Car.rb
@@ -0,0 +1,59 @@
+class Car
+  attr_accessor :counter, :fuel # Create get and set methods for attrs
+
+  def drive(l)
+    counter = l
+    fuel_change(2)
+  end
+
+  # define private method
+  private
+    def fuel_change(v)
+      fuel = v
+    end
+
+end
+
+class Vios < Car
+  attr_accessor :wheel
+  
+  def initialize()
+    @wheel = 4 #Instance variable
+    @utility = ""
+    @@seat = 5
+  end
+  
+  def setUtility(v)
+    @utility = v
+  end
+  
+  def getUtility
+    @utility
+  end
+
+  def self.seat
+    @@seat
+  end
+
+  def self.seat=(s)
+    @@seat = s
+  end
+
+end
+
+class Vios3_0 < Vios
+  def drive(l)
+    super # Call parent drive() method
+    puts "GPS"
+  end
+end
+
+v2 = Vios3_0.new
+v2.drive(500)
+v2.setUtility("GPS")
+puts v2.wheel
+puts v2.getUtility
+v3 = Vios3_0.new
+v3.setUtility("GPS1")
+puts v3.getUtility
+puts Vios3_0.seat
