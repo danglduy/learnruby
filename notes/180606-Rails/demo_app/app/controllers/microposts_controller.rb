c53e085cd9a1325717151ab30c915392ef78f3b6
zldang <38782010+zldang@users.noreply.github.com>
2018-06-10 21:38:18 +0700

    Reformat

--- notes/180606-Rails/demo_app/app/controllers/microposts_controller.rb
+++ notes/180606-Rails/demo_app/app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :set_micropost, only: [:show, :edit, :update, :destroy]
+  before_action :set_micropost, only: %i[show edit update destroy]
 
   # GET /microposts
   # GET /microposts.json
@@ -9,8 +9,7 @@ class MicropostsController < ApplicationController
 
   # GET /microposts/1
   # GET /microposts/1.json
-  def show
-  end
+  def show; end
 
   # GET /microposts/new
   def new
@@ -18,8 +17,7 @@ class MicropostsController < ApplicationController
   end
 
   # GET /microposts/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /microposts
   # POST /microposts.json
@@ -28,7 +26,7 @@ class MicropostsController < ApplicationController
 
     respond_to do |format|
       if @micropost.save
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
+        format.html { redirect_to @micropost, notice: "Micropost was successfully created." }
         format.json { render :show, status: :created, location: @micropost }
       else
         format.html { render :new }
@@ -42,7 +40,7 @@ class MicropostsController < ApplicationController
   def update
     respond_to do |format|
       if @micropost.update(micropost_params)
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully updated.' }
+        format.html { redirect_to @micropost, notice: "Micropost was successfully updated." }
         format.json { render :show, status: :ok, location: @micropost }
       else
         format.html { render :edit }
@@ -56,19 +54,20 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     respond_to do |format|
-      format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
+      format.html { redirect_to microposts_url, notice: "Micropost was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_micropost
-      @micropost = Micropost.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def micropost_params
-      params.require(:micropost).permit(:content, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_micropost
+    @micropost = Micropost.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def micropost_params
+    params.require(:micropost).permit(:content, :user_id)
+  end
 end
