a3145a71ab21f37b459d6015849c3f0c31b7ea05
zldang <38782010+zldang@users.noreply.github.com>
2018-06-13 21:05:35 +0700

    180613

---
+++ notes/180608-Rails/sample_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def new
+    render 'new'
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # not @user because local var
+
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    flash.now[:warning] = 'You have been logged out'
+    render 'new'
+  end
+end
--- notes/180608-Rails/sample_app/app/controllers/users_controller.rb
+++ notes/180608-Rails/sample_app/app/controllers/users_controller.rb
@@ -8,8 +8,32 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-
+  def index
+    @user = User.all
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!" # flash : appcontroller method
+
+      # redirect_to user_path(id: @user.id)
+      # redirect_to ('/users/'+@user.id.to_s)
+      # redirect_to user_url(@user)
+
+      redirect_to @user
+
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                      :password_confirmation)
+
+    end
 
 end
