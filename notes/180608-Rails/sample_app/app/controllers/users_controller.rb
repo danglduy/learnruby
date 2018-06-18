3185ebe2efba929920900d6b0a2faedc0ed0bc2b
zldang <38782010+zldang@users.noreply.github.com>
2018-06-18 21:12:39 +0700

    180618

--- notes/180608-Rails/sample_app/app/controllers/users_controller.rb
+++ notes/180608-Rails/sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,   only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -11,20 +12,21 @@ class UsersController < ApplicationController
   end
 
   def index
-    @user = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!" # flash : appcontroller method
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
 
       # redirect_to user_path(id: @user.id)
       # redirect_to ('/users/'+@user.id.to_s)
       # redirect_to user_url(@user)
 
-      redirect_to @user
+
 
     else
       render 'new'
@@ -48,7 +50,11 @@ class UsersController < ApplicationController
     end
   end
 
-
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -58,6 +64,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please login!"
         redirect_to login_url
       end
@@ -68,6 +75,10 @@ class UsersController < ApplicationController
       unless @user == current_user
         redirect_to root_path
       end
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
--- notes/180608-Rails/sample_app/config/application.rb
+++ notes/180608-Rails/sample_app/config/application.rb
@@ -14,5 +14,12 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+    config.action_mailer.smtp_settings = {
+
+    }
   end
 end
