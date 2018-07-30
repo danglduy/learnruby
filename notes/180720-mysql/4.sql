c57b14d94e62a4ca3f1b4f41e135696d3b08ef60
zldang <38782010+zldang@users.noreply.github.com>
2018-07-30 19:54:27 +0700

    sql4 fix

--- notes/180720-mysql/4.sql
+++ notes/180720-mysql/4.sql
@@ -9,3 +9,15 @@ WHERE s.id IN (
     WHERE c.teacher_id = 1
   ) AND r.student_id = 1
 );
+
+
+SELECT s.name
+FROM subject s
+WHERE s.id IN (
+  SELECT r.subject_id
+  FROM register r
+  INNER JOIN class c
+  ON r.class_id = c.id
+  WHERE r.student_id = 1
+  AND c.teacher_id = 1
+)
